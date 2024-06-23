5fcc2380ec20b0e1442a08d0e87dc703a0a79e5c
statueofdavid <david@declared.space>
2024-06-23 11:05:35 -0400

    batt low

--- meteo.js
+++ meteo.js
@@ -1,9 +1,28 @@
-const url = 'https://api.open-meteo.com/v1/forecast?latitude=37.24568&longitude=-76.78029&current=temperature_2m,is_day,precipitation,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,pressure_msl,surface_pressure,cloud_cover,visibility&daily=sunrise,sunset,daylight_duration,uv_index_max,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York';
+const url = 'https://api.open-meteo.com/v1/forecast?'
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    
-    
-  })
+const parameters = {
+	latitude: '37.248096',
+	longitude: '-76.791190',
+	temperature_unit: 'fahrenheit',
+	wind_speed_unit: 'mph',
+	precipitation_unit: 'inch',
+	timezone: 'America/New_York',
+	current: 'temperature_2m,is_day,precipitation,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m',
+	hourly: 'temperature_2m,relative_humidity_2m,precipitation_probability,pressure_msl,surface_pressure,cloud_cover,visibility',
+	daily: 'sunrise,sunset,daylight_duration,uv_index_max,precipitation_probability_max'
+};
+
+const urlParameters = new URLSearchParams(parameters);
+
+console.log('calling openMeteo api');
+
+fetch(url + urlParameters)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      
+      
+    })
+
+
+
