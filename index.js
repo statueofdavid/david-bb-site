2b25ffbf22f7d855c6e2ed1496f851cf61666982
sodvm <dmpersonalemail@gmail.com>
2024-07-09 12:50:56 -0400

    lunchtime

--- index.js
+++ index.js
@@ -1,55 +1,4 @@
 
-
-const header = document.querySelector('header');
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const menuContent = document.querySelector('.menu-content');
-
-hamburgerMenu.addEventListener('click', () => {
-  let invisible = getComputedStyle(menuContent).display;
-  console.log(invisible)
-
-  const modalHeader = document.getElementById("modal-header");
-  const modalText = document.getElementById("modal-text");
-  
-  if (invisible) {
-    menuContent.classList.toggle('show');  // Toggle 'show' class for visibility
-  } else {
-    menuContent.classList.toggle('none');
-  }
-});
-
-const footer = document.querySelector('footer');
-
-const greyMatterPath = document.getElementById("grey-matter");
-const greyMatterSvg = greyMatterPath.getAttribute('d');
-console.log(`SVG d string: ${greyMatterSvg}`);
-
-greyMatterPath.addEventListener("click", function() {
-  console.log("clicked"); 
-  showModal();
-});
-
-const modal = document.getElementById("modal");
-const modalHeader = document.getElementById("modal-header");
-const modalText = document.getElementById("modal-text");
-
-function showModal() {
-  modalHeader.innerHTML = `This is my Brain on the Internet`;
-  modalText.innerHTML += `You clicked!`;
-  modal.style.display = "block"; 
-  header.style.display = 'none';
-  footer.style.display = 'none';
-}
-
-// Function to close the modal when the button is clicked
-function closeModal() {
-  modal.style.display = "none"; // Hide modal
-  modalText.innerHTML = "";
-  
-  header.style.display = 'block';
-  footer.style.display = 'block';
-}
-
 /* Some Typed Thoughts for the brain's behavior 
  * 1. It will have brain sections 
  * 2. If user's cursor IS NOT on over the Grey Matter 
@@ -67,3 +16,53 @@ function closeModal() {
  *    i. emit a pixel 1/2 the received if not having previously received a pixel 
  *    ii. emit a pixel 2x the received if this region is the origin of the pixel 
  **/
+
+const footer = document.querySelector('footer');
+const greyMatterPath = document.getElementById("grey-matter");
+const greyMatterSvg = greyMatterPath.getAttribute('d');
+
+const header = document.querySelector('header');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menuContent = document.querySelector('.menu-content');
+
+const modal = document.getElementById("modal");
+const modalHeader = document.getElementById("modal-header");
+const modalText = document.getElementById("modal-text");
+
+// Hamburger Menu Behavior
+console.log(`MenuContent: ${menuContent}`);
+hamburgerMenu.addEventListener('click', () => {
+  if (menuContent.style.visibility == 'hidden') {
+    menuContent.style.visibility = 'visible'; 
+    console.log(menuContent.style.visibility);
+  } else {
+    menuContent.style.visibility = 'hidden';
+    console.log(menuContent.style.visibility);
+  }
+});
+
+// Brain Outline Behavior
+console.log(`SVG d string: ${greyMatterSvg}`);
+greyMatterPath.addEventListener("click", function() {
+  console.log("clicked"); 
+  showModal();
+});
+
+/* helper functions */
+// overlays the modal over header,main,footer
+function showModal() {
+  modalHeader.innerHTML = `This is my Brain on the Internet`;
+  modalText.innerHTML += `You clicked!`;
+  modal.style.display = "block"; 
+  header.style.display = 'none';
+  footer.style.display = 'none';
+}
+
+// removes the modal
+function closeModal() {
+  modal.style.display = "none"; // Hide modal
+  modalText.innerHTML = "";
+  
+  header.style.display = 'block';
+  footer.style.display = 'block';
+}
